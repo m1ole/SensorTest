b8d960bd6c2580548b0f562d2bb7c1284dcd0d69
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-03-24 18:08:42 +0200

    Initial Commit

---
+++ Shared/TestSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSensorApp.swift
+//  Shared
+//
+//  Created by iosdev on 24.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
