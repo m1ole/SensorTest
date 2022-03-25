ea1ab477ab553954d8fbcb472edcf26046e1a2a7
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-03-25 12:30:19 +0200

    Basic bluetooth reader

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,16 +1,58 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by iosdev on 24.3.2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var bleManager = BLEManager()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Spacer()
+        Text("Bluetooth Devices")
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity, alignment: .center)
+        List(bleManager.peripherals) { peripheral in
+            HStack {
+                Text(peripheral.name)
+                Spacer()
+                Text(String(peripheral.rssi))
+            }
+        }.frame(height: 300)
+        
+        Spacer()
+        
+        Text("STATUS")
+            .font(.headline)
+        
+        if bleManager.isSwitchedOn {
+            Text("Bluetooth is switched on")
+                .foregroundColor(.green)
+        }
+        else {
+            Text("Bluetooth is NOT switched on")
+                .foregroundColor(.red)
+        }
+        
+        Spacer()
+        
+        HStack {
+            VStack (spacing: 10) {
+                Button(action: {
+                    self.bleManager.startScanning()
+                }) {
+                    Text("Start Scanning")
+                }
+                
+            }.padding()
+            
+            Spacer()
+            
+            VStack (spacing: 10) {
+                Button(action: {
+                    self.bleManager.stopScanning()
+                }) {
+                    Text("Stop Scanning")
+                }
+            }.padding()
+        }
+        Spacer()
+        
     }
 }
 
--- Shared/TestSensorApp.swift
+++ Shared/TestSensorApp.swift
@@ -1,10 +1,3 @@
-//
-//  TestSensorApp.swift
-//  Shared
-//
-//  Created by iosdev on 24.3.2022.
-//
-
 import SwiftUI
 
 @main
