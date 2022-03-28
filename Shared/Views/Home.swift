698f37269e7043a5e3195fff0984f1810cfa4cc8
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-03-28 12:34:47 +0300

    Home view

---
+++ Shared/Views/Home.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+struct Home: View {
+    @ObservedObject var bleManager = BLEManager()
+    
+    var body: some View {
+        
+        VStack {
+            Text("Bluetooth Devices")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .center)
+            List(bleManager.peripherals) { peripheral in
+                HStack {
+                    Text(peripheral.name)
+                    Spacer()
+                    Text(String(peripheral.rssi))
+                }
+            }.frame(height: 300)
+            
+            Spacer()
+            
+            Text("STATUS")
+                .font(.headline)
+            
+            if bleManager.isSwitchedOn {
+                Text("Bluetooth is switched on")
+                    .foregroundColor(.green)
+            }
+            else {
+                Text("Bluetooth is NOT switched on")
+                    .foregroundColor(.red)
+            }
+            
+            Spacer()
+            
+            HStack {
+                VStack (spacing: 10) {
+                    Button(action: {
+                        self.bleManager.startScanning()
+                    }) {
+                        Text("Start Scanning")
+                    }
+                    
+                }.padding()
+
+                VStack (spacing: 10) {
+                    Button(action: {
+                        self.bleManager.stopScanning()
+                    }) {
+                        Text("Stop Scanning")
+                    }
+                }.padding()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
